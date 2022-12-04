5c74055e6290b0480bd41fd9174ff7611b24d4c5
Ricardotyr <r.sanmartinneira@gmail.com>
2022-12-03 23:44:56 -0300

    Static_y_Carro

--- Tragon/settings.py
+++ Tragon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^dd$_9*24%1%x+hythio&rnvvezmqctm3kq=cn!fm=c5+!6la('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,8 +130,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+# para los ccs y todo lo demas 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Tragon/static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
@@ -144,3 +150,4 @@ EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="tragonballzpyme@gmail.com"
 EMAIL_HOST_PASSWORD="rxdfagvcztqntauv"
+CSRF_TRUSTED_ORIGINS = ['https://tragonballz.azurewebsites.net/']
--- Tragon/urls.py
+++ Tragon/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('TragonBall.urls')),
     path('carro/', include('carro.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
